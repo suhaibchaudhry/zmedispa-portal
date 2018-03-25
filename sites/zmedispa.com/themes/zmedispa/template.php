4748269df42c05bf2ccfc5ebd0667265e798ed85
Suhaib Chaudhry <suhaib@uitoux.com>
2018-03-25 13:19:08 -0400

    menu

--- sites/zmedispa.com/themes/zmedispa/template.php
+++ sites/zmedispa.com/themes/zmedispa/template.php
@@ -3,6 +3,10 @@ function zmedispa_superfish_menu_item_link($variables) {
 //dpm($variables);  
 $menu_item = $variables['menu_item'];
 $link_options = $variables['link_options'];
-$link = l($menu_item['link']['title']."<span>".$variables['link_options']['attributes']['title']."</span>", $menu_item['link']['href'], array('html' => true));
+if($menu_item['link']['title'] == 'Store') {
+	$link = l($menu_item['link']['title']."<span>".$variables['link_options']['attributes']['title']."</span>", $menu_item['link']['href'], array('html' => true, 'attributes' => array('target' => '_blank')));
+} else {
+	$link = l($menu_item['link']['title']."<span>".$variables['link_options']['attributes']['title']."</span>", $menu_item['link']['href'], array('html' => true));
+}
 return $link;
 }
