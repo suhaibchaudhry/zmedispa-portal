fab9c5e2799bb82f90ec603c8bc116a6d031631b
Suhaib Chaudhry <suhaib@uitoux.com>
2018-06-10 19:25:01 -0400

    Ahmed hack

--- sites/all/themes/omega/omega/templates/section--header.tpl.php
+++ sites/all/themes/omega/omega/templates/section--header.tpl.php
@@ -1,3 +1,8 @@
 <header<?php print $attributes; ?>>
   <?php print $content; ?>
+  <style type="text/css">
+  	.captcha .in-field-labels-processed{
+    	position: relative !important;
+	}
+  </style>
 </header>
